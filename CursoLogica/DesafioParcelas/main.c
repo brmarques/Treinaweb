0d9295160cbbd32c256dc8d3137a984bdf8ecf28
Braulio <braulio007@gmail.com>
2021-03-01 20:09:14 -0300

    Update main.c

--- CursoLogica/DesafioParcelas/main.c
+++ CursoLogica/DesafioParcelas/main.c
@@ -4,21 +4,21 @@
 int main()
 {
     float parcelaInicial;
-    float porcentagemDecrecimo;
+    float porcentagemDecrescimo;
     float qtdParcelas;
     int n = 1;
     float valorParcela;
     printf("Digite o valor da parcela inicial: ");
     scanf("%f", &parcelaInicial);
     printf("Digite o valor da porcentagem a ser subtraida a cada parcela: ");
-    scanf("%f", &porcentagemDecrecimo);
+    scanf("%f", &porcentagemDecrescimo);
     printf("Digite o valor da quantidade de parcelas: ");
     scanf("%f", &qtdParcelas);
     printf("%d - %.2f \n", n, parcelaInicial);
 
     for(int i = qtdParcelas; i>1; i--){
         n++;
-        valorParcela = parcelaInicial- (parcelaInicial * (porcentagemDecrecimo/100));
+        valorParcela = parcelaInicial- (parcelaInicial * (porcentagemDecrescimo/100));
         printf("%d - %.2f \n", n, valorParcela);
         parcelaInicial = valorParcela;
     }
